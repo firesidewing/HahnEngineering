70047c162fa45f9e652e8a3980ece997c8f4fce9
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2021-09-23 18:08:26 -0700

    formatting, subscribe

--- home/models.py
+++ home/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
 
 class HomePage(Page):
@@ -20,4 +27,28 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         ImageChooserPanel('image'),
         FieldPanel('body', classname="full"),
+    ]
+
+
+class SubscribeField(AbstractFormField):
+    page = ParentalKey('SubscribePage', on_delete=models.CASCADE,
+                       related_name='form_fields')
+
+
+class SubscribePage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Subscribe fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
